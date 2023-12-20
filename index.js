b8274cf5205ee0a777635574a66bf27d02f490cb
Utsav Redekar <utsavredekar28@gmail.com>
2023-12-20 17:29:43 +0530

    last updated

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ function expandBox(e){
     e.classList.toggle('expanded')
 }
  
+function addToCart() {
+    const selectedOptions = [];
+    const boxes = ['box1', 'box2', 'box3'];
+
+    boxes.forEach(boxId => {
+      const selectedOption = document.querySelector(`input[name=${boxId}]:checked)`);
+      if (selectedOption) {
+        selectedOptions.push(`${boxId}: ${selectedOption.nextSibling.textContent.trim()}`);
+      }
+    });
+
+    if (selectedOptions.length > 0) {
+        alert(`Added to Cart:\n${selectedOptions.join('\n')}`);
+      } else {
+        alert('Please select options before adding to cart.');
+      }
+}
 
